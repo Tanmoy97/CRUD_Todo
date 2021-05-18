7c2c246e2c0c3feaa45da6958e47cbe00e08cac1
Tanmoy khatua <53449205+Tanmoy97@users.noreply.github.com>
2021-05-18 15:58:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Tabs from "./component/TabMenu/Tabs";
+//import GalleryReact from "./component/TabMenu/GalleryReact";
+import Todo from "./component/todoProject/todo";
+
+const App = () => {
+  return (
+    <>
+     <Todo />
+    
+    </>
+  )
+}
+
+export default App
